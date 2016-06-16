e2dcfdc8f4b985cf63024418c9eae765ac152961
bladespinner <bladespinner@gmail.com>
2016-06-16 20:26:13 +0300

    Add SVD algorithm

--- src/songRecommender/songRecommender.py
+++ src/songRecommender/songRecommender.py
@@ -1,6 +1,13 @@
+import recsys.algorithm
 from recsys.algorithm.factorize import SVD
 
-svd = SVD()
-svd.load_data(filename='./data/movielens/ratings.dat',
-            sep='::',
+recsys.algorithm.VERBOSE = True
+
+svd = SVDNeighbourhood()
+svd.load_data(filename='../data/traindata.dat',
+            sep=',',
             format={'col':0, 'row':1, 'value':2, 'ids': int})
+
+k = 100
+svd.compute(k=k, min_values=1, pre_normalize=None, mean_center=True, post_normalize=True)
+print(svd.recommend(1, only_unknowns=False, is_row=False))
